da2ff8f4e9a6341e185a00dee156fbeb7b18c362
brownesc <andresuncle@gmail.com>
2019-08-12 02:32:09 -0400

    updates

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
+const knex = require('knex')
 
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'postgres',
+      database : 'facerecognition'
+    }
+  });
+
+db.select('*').from('users').then(data => {
+    console.log(data);
+});
 const app = express();
 
 app.use(bodyParser.json())
@@ -40,45 +54,41 @@ app.post('/signin',(req,res)=>{
     }
 })
 app.post('/register',(req,res)=>{
-    const{ email, name, password}= req.body
-    database.users.push({
-        id: '123',
+    const{ email, name, password}= req.body;
+    db('users').returning('*').insert({
+        email: email,
         name: name,
-        email:  email,        
-        entries: 0,
         joined: new Date()
+    }).then(user => {
+        res.json(user[0]);
     })
-    res.json(database.users[database.users.length-1])
+    .catch(err => res.status(400).json('unable to register'))
+    
+    
     
 })
 app.get('/profile/:id', (req,res) => {
     const {id}=req.params;
-    let found=false;
-    database.users.forEach(user =>{
-        if (user.id===id){
-            found=true;
-            return res.json(user);
-            
-        }
+    db.select('*').from('users').where({
+        id: id
     })
-    if (!found) {
-        res.status(400).json('not found')
-    }
+    .then(user=> {
+        if (user.length){
+            res.json(user[0]);
+        }else{res.status(400).json('not found')
+        }    
+    })
+    .catch(err => res.status(400).json('error getting user'))
 })
 app.put('/image',(req, res)=> {
     const {id}=req.body;
-    let found=false;
-    database.users.forEach(user =>{
-        if (user.id===id){
-            user.entries++;
-            found=true;
-            return res.json(user.entries);
-        }
+    db('users').where('id','=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries =>{
+        res.json(entries[0]);
     })
-    if (!found) {
-        res.status(400).json('not found')
-    }
-
+    .catch(err => res.status(400).json('unable to get count'))      
 })
 app.listen(3000, ()=> {
  console.log('app is running on port 3000 bitches');
